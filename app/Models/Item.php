f6985064b7dcdabe8128d1dc9934a6c719de729d
Saya <sayamaeda0538@gmail.com>
2024-07-12 23:28:41 +0900

    [model/item]
    added three relationships to user, category, and comment

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comment()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
 }
