48756899a31afe7a1da0e689c17b7f173b42ba95
Masaya Sato <masaput1282@yahoo.co.jp>
2024-07-12 21:28:57 +0900

    [UI / modal] - Create modal template
    - Create modal template for each delete modal

---
+++ resources/views/users/modal/sample.blade.php
@@ -0,0 +1,57 @@
+<div class="modal fade" id="#">
+    <div class="modal-dialog modal-dialog-scrollable custom-modal" role="document">
+        <div class="modal-content">
+            <div class="modal-header border-0">
+            </div>
+            <div class="modal-body border-0">
+                <h2 class="modal-title text-danger text-center d-flex align-items-center justify-content-center" id="modalTitleId">
+                    <i class="fa-solid fa-trash-can icon-md me-3"></i> Delete # your part #
+                </h2>
+                <p class="text-center letter-size">Are you sure to delete # your part #?</p>
+            </div>
+            <div class="modal-footer border-0 d-flex justify-content-center">
+                <button type="button" class="btn btn-outline-danger btn-rounded" data-bs-dismiss="modal">
+                    Cancel
+                </button>
+                <button type="button" class="btn btn-danger btn-rounded">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+{{-- style.css --}}
+
+{{-- .custom-modal .modal-content {
+    border-radius: 30px;
+    overflow: hidden;
+}
+
+.custom-modal .modal-content::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    pointer-events: none;
+    border-radius: 50px;
+}
+
+.letter-size {
+    font-size: 1rem;
+}
+
+.btn-rounded {
+    border-radius: 50px;
+    padding: 5px;
+    font-weight: bold;
+    font-size: 1rem;
+}
+
+.btn-danger:hover {
+    background-color: transparent;
+    color: red;
+    border-color: red;
+    border: 2px solid red;
+} --}}
