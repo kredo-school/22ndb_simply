272867aee9ffb663840dbd7004a358f2f42ef4d6
Saya <sayamaeda0538@gmail.com>
2024-07-12 23:22:30 +0900

    [seeder/category-seeder]
    inserted category data into database for test ui of homepage

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    private $category;
+
+    public function __construct(Category $category){
+        $this->category = $category;
+    }
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $category = [
+            [
+                "name" => "Clothes",
+                "created_at" => NOW(),
+                "updated_at" => NOW(),
+            ],
+            
+            [
+                "name" => "Kitchen",
+                "created_at" => NOW(),
+                "updated_at" => NOW(),
+            ],
+
+            [
+                "name" => "Garage",
+                "created_at" => NOW(),
+                "updated_at" => NOW(),
+            ]
+            
+        ];
+
+        $this->category->insert($category);
+       
+    }
+}
