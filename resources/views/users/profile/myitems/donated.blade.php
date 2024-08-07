27859c66b0963d9e8fad80162be939a6f173fc23
Ami <ami1018amc@gmail.com>
2024-08-07 12:34:37 +0900

    [UI/myitems] Modified -forelse in myitems.blade

--- resources/views/users/profile/myitems/donated.blade.php
+++ resources/views/users/profile/myitems/donated.blade.php
@@ -3,15 +3,17 @@
 @section('items-content')
     <div class="container">
         <div class="row row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
-            @foreach($donatedItems as $donatedItem)
-            <div class="col d-flex flex-column align-items-center mb-3 h6">
-{{-- Add image link --}}
-                    <a href="">
-                        <img src="{{$donatedItem->item->image}}" alt="{{$donatedItem->item->name}}" class="img-item-md mb-3">
-                    </a>
-                    <span>{{$donatedItem->item->name}}</span>
-            </div>
-            @endforeach
+            @forelse($donatedItems as $donatedItem)
+                <div class="col d-flex flex-column align-items-center mb-3 h6">
+    {{-- Add image link --}}
+                        <a href="">
+                            <img src="{{$donatedItem->item->image}}" alt="{{$donatedItem->item->name}}" class="img-item-md mb-3">
+                        </a>
+                        <span>{{$donatedItem->item->name}}</span>
+                </div>
+            @empty
+                <p class="h6 color-gray-1">Donated items list...</p>
+            @endforelse
         </div>
     </div>
 @endsection
--- resources/views/users/profile/myitems/favorites.blade.php
+++ resources/views/users/profile/myitems/favorites.blade.php
@@ -5,28 +5,29 @@
 
         <div class="row row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
             {{-- add @foreach --}}
-            @foreach($favoriteItems as $favoriteItem)
-            <div class="col d-flex flex-column align-items-center mb-3 h6">
-                <div class="align-items-start ">
-{{-- Add image link --}}
-                    <a href="">
-                        <img src="{{$favoriteItem->donationItem->item->image}}" alt="{{$favoriteItem->donationItem->item->name}}" class="img-item-md">
-                    </a>
-                    <div class="mt-1 d-flex justify-content-between">
-                        <span class="color-gray-1">{{$favoriteItem->donationItem->item->category->name}}</span>
-                        &nbsp;
-                        <i class="fa-solid fa-bookmark"></i>
-                    </div>
-                    <span>{{$favoriteItem->donationItem->item->name}}</span>
-{{-- Add username link --}}
-                    <div class="">
-                        <a href="" class="text-dark">{{$favoriteItem->donationItem->item->user->username}}</a>
+            @forelse($favoriteItems as $favoriteItem)
+                <div class="col d-flex flex-column align-items-center mb-3 h6">
+                    <div class="align-items-start ">
+    {{-- Add image link --}}
+                        <a href="">
+                            <img src="{{$favoriteItem->donationItem->item->image}}" alt="{{$favoriteItem->donationItem->item->name}}" class="img-item-md">
+                        </a>
+                        <div class="mt-1 d-flex justify-content-between">
+                            <span class="color-gray-1">{{$favoriteItem->donationItem->item->category->name}}</span>
+                            &nbsp;
+                            <i class="fa-solid fa-bookmark"></i>
+                        </div>
+                        <span>{{$favoriteItem->donationItem->item->name}}</span>
+    {{-- Add username link --}}
+                        <div class="">
+                            <a href="" class="text-dark">{{$favoriteItem->donationItem->item->user->username}}</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            @endforeach
+            @empty
+                <p class="h6 color-gray-1">Favorite items list...</p>
+            @endforelse
 
-            
         </div> 
     </div>
 @endsection
