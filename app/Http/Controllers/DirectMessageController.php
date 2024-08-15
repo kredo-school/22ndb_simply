05c0ddb64c93cc947d2688baf247d610d9035730
Masaya Sato <masaput1282@yahoo.co.jp>
2024-08-15 19:06:56 +0900

    [Controller/DirectMessage] Edit functions and adjust styles Changes:
    Add comments and query on DirectMessageController
    Add nullable on items and direct_message table
    Edit color to adjust team used color

--- app/Http/Controllers/DirectMessageController.php
+++ app/Http/Controllers/DirectMessageController.php
@@ -21,8 +21,11 @@ class DirectMessageController extends Controller
         $sender_id = Auth::id();
         $sender    = User::findOrFail($sender_id);
         $recipient = User::findOrFail($id);
+
+        // get all other users
         $all_users = User::where('id', '!=', $sender_id)
                         ->where(function($query) use ($sender_id) {
+                            // find all users who have talked to the sender in their direct or received messages
                             $query->whereHas('directMessages', function($query) use ($sender_id) {
                             $query->where('user_id', $sender_id)
                         ->orWhere('destination_user_id', $sender_id);
@@ -31,22 +34,34 @@ class DirectMessageController extends Controller
                             $query->where('user_id', $sender_id)
                         ->orWhere('destination_user_id', $sender_id);
                         });
-                    })->get();
+                    })
+                    ->withMax('directMessages', 'created_at')
+                    ->withMax('receivedMessages', 'created_at')
+                    ->get();
 
+        // count how many messages the user hasn't read yet
+        //
         foreach ($all_users as $user) {
             $user->unread_count = DirectMessage::unreadCount($user->id, $sender_id);
+            $user->latest_message_date = max($user->direct_messages_max_created_at, $user->received_messages_max_created_at);
         }
 
+        // Sort users by the latest message date
+        $all_users = $all_users->sortByDesc('latest_message_date');
+
+        //  get all messages sent between two specific people, from oldest to newest
         $messages  = DirectMessage::with('user')->where(function($query) use ($sender_id, $id) {
             $query->where([
-                ['user_id', $sender_id],
-                ['destination_user_id', $id]
-            ])->orWhere([
-                ['user_id', $id],
-                ['destination_user_id', $sender_id]
-            ]);
+                        ['user_id', $sender_id],
+                        ['destination_user_id', $id]
+                    ])->orWhere([
+                        ['user_id', $id],
+                        ['destination_user_id', $sender_id]
+                    ]);
         })->orderBy('created_at', 'asc')->get();
 
+        // if Auth user is the recipient and there are unread messages, they will be marked as read
+        // the figure changed 0 to 1
         DirectMessage::where('user_id', $id)
                         ->where('destination_user_id', $sender_id)
                         ->where('seen', false)
@@ -61,11 +76,21 @@ class DirectMessageController extends Controller
 
     public function store(Request $request, $recipient_id)
     {
-        $directMessage = new DirectMessage;
-        $directMessage->text                = $request->text;
+        $directMessage = new DirectMessage();
+
         $directMessage->user_id             = Auth::id() ;
         $directMessage->destination_user_id = $recipient_id;
         $directMessage->seen                = false;
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('images'), $imageName);
+            $directMessage->image = 'images/' . $imageName;
+        } else {
+            $directMessage->text = $request->text;
+        }
+
         $directMessage->save();
 
         return redirect()->route('directMessage.show', ['id' => $recipient_id]);
--- database/migrations/2024_07_05_131030_create_direct_messages_table.php
+++ database/migrations/2024_07_05_131030_create_direct_messages_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('direct_messages', function (Blueprint $table) {
             $table->id();
             $table->string('text');
+            $table->string('image')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('destination_user_id');
             $table->boolean('seen')->default(false);
