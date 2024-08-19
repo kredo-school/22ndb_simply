e7ade8adc98bd08c0cb75c54bbcdca866a89020b
Saya <sayamaeda0538@gmail.com>
2024-08-19 22:02:28 +0900

    [backend/each-category-page]
    Modified the routes for delete category

--- routes/web.php
+++ routes/web.php
@@ -71,7 +71,7 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('other_items/{id}', [ItemController::class, 'otherItemPage'])->name('other_items');
     Route::patch('/edit/category/{id}', [CategoryController::class, 'editCategory'])->name('edit.category');
     Route::post('/create/category', [CategoryController::class, 'createCategory'])->name('create.category');
-    Route::delete('/delete/category/{id}', [CategoryController::class, 'deleteCategory'])->name('delete.category');
+    Route::delete('/delete/category/{id}', [CategoryController::class, 'destroy'])->name('delete.category');
 
 
     // Donated-item
